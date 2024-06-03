3298205bd4fc0cecf29dae4504c463e895ad49b6
vaammpyy <rohankumarprasad@yahoo.com>
2024-06-03 10:27:56 -0400

    modifying the DS of the lc object

--- TESSLC_class.py
+++ TESSLC_class.py
@@ -30,45 +30,33 @@ class TESSLC:
     class LC:
         def __ini__(self):
             self.full=NONE
-            self.segmented=NONE
+            self.segment=NONE
 
-        #masks LC with flags array
-        def mask_lc(self,flags,series_name):
-            data=getattr(self,series_name)
-            if len(data)>1:
-                for i in range(len(data)):
-                    mask=np.isin(data[i][0]["quality"],np.asarray(flags))
 
-                    data_dict={"time":data[i][0]["time"][mask],
-                               "flux":data[i][0]["flux"][mask],
-                               "flux_err":data[i][0]["flux_err"][mask],
-                               "quality":data[i][0]["quality"][mask],
-                               "start_index":0,
-                               "stop_index": len(data[i][0]["time"]),
-                               "flags":flags}
-
-                    data[i].append(data_dict)
-                    setattr(self,series_name,data)
-            else:
-                mask=np.isin(data[0]["quality"],np.asarray(flags))
-                data_dict={"time":data[0]["time"][mask],
-                           "flux":data[0]["flux"][mask], 
-                           "flux_err":data[0]["flux_err"][mask], 
-                           "quality":data[0]["quality"][mask], 
-                           "start_index":0, 
-                           "stop_index": len(data[0]["time"]), 
-                           "flags":flags}
-                data.append(data_dict)
-                setattr(self,series_name,data)
-
-        def gls(self,series_name="full",flag_series=0):
-            data=getattr(self,series_name)
-            index=flag_series
-            time=data[index]['time']
-            flux=data[index]['flux']
-            flux_err=data[index]['flux_err']
+        def gls(self,mask):
+            data=self.full
+            time=data['time'][mask]
+            flux=data['flux'][mask]
+            flux_err=data['flux_err'][mask]
             result=xo.lomb_scargle_estimator(time, flux, yerr=flux_err, max_peaks=1, min_period=0.01, max_period=200.0, samples_per_peak=50)
             return result['peaks'][0]['period']
+        
+        #masks LC with flags array
+        def get_mask(self, q_flags=None, segments=None):
+            
+            if q_flags is None:
+                q_mask=np.ones_like(self.full['time'], dtype=bool)
+            else:
+                q_mask=np.isin(self.full['quality'],np.asarray(q_flags))
+
+            if segments is None:
+                seg_mask=np.ones_like(self.full['time'], dtype=bool)
+            else:
+                seg_mask=np.isin(self.segment,np.asarray(segments))
+
+            mask= q_mask & seg_mask
+
+            return(mask)
 
     class STAR:
         pass
@@ -85,13 +73,10 @@ class TESSLC:
         lc=search_lc.download(quality_bitmask=0)
 
         # Making the raw lightcurve file
-        self.lc.full=[{"time":lc['time'].value.astype(np.float64),
+        self.lc.full={"time":lc['time'].value.astype(np.float64),
                        "flux":np.array(lc['flux'].value,dtype=np.float64),
                        "flux_err": np.array(lc['flux_err'].value,dtype=np.float64),
-                       "quality": np.ma.getdata(lc["quality"]),
-                       "start_index":0,
-                       "stop_index":len(lc["time"].value),
-                       "flags": np.nan}]
+                       "quality": np.ma.getdata(lc["quality"])}
 
         # Assigning the stellar properties
         self.star.ra_obj=lc.meta['RA_OBJ']
@@ -120,37 +105,43 @@ class TESSLC:
 
     #Segmenting LC with conditions, t_gap>period, t_segment>2hr
     def segment_lc(self):
-        self.lc.segmented=[]
-        period=self.lc.gls(series_name="full", flag_series=1)
+        # self.lc.segmented=[]
+        mask=self.lc.get_mask(q_flags=[0])
+        period=self.lc.gls(mask)
 
-        t_gap= np.where(np.diff(self.lc.full[0]['time'])>self.inst.cadence+self.inst.cadence_err)[0]
+        t_gap= np.where(np.diff(self.lc.full['time'])>self.inst.cadence+self.inst.cadence_err)[0]
+
         segment=[]
 
         for i in range(len(t_gap)):
-            t_i=self.lc.full[0]['time'][t_gap[i]]
-            t_i_1=self.lc.full[0]['time'][t_gap[i]+1]
+            t_i=self.lc.full['time'][t_gap[i]]
+            t_i_1=self.lc.full['time'][t_gap[i]+1]
             diff_t=t_i_1-t_i
             if diff_t>period:
-               segment.append(t_gap[i]) 
+                segment.append(t_gap[i]) 
 
         segment=np.insert(segment,0,-1)
-        segment=np.append(segment,len(self.lc.full[0]['time'])-2)
+        segment=np.append(segment,len(self.lc.full['time'])-1)
 
         min_segment_length=0.0833 # defining the threshold segment lenght of two hours i.e. 1/12 [d]
 
+        segment_mask=np.zeros(len(self.lc.full['time']))
+
+        segment_number=1
         for i in range(1,len(segment)):
-            segment_dict={"time":None, 
-                          "flux":None, 
-                          "flux_err":None, 
-                          "quality":None, 
-                          "start_index":None, 
-                          'stop_index':None, 
-                          "flags":None}
-            if self.lc.full[0]['time'][segment[i]]-self.lc.full[0]['time'][segment[i-1]+1]>min_segment_length:
-                segment_dict['time']=self.lc.full[0]["time"][segment[i-1]+1:segment[i]+1] 
-                segment_dict['flux']=self.lc.full[0]["flux"][segment[i-1]+1:segment[i]+1] 
-                segment_dict['flux_err']=self.lc.full[0]["flux_err"][segment[i-1]+1:segment[i]+1] 
-                segment_dict['quality']=self.lc.full[0]["quality"][segment[i-1]+1:segment[i]+1] 
-                segment_dict['start_index']=segment[i-1]+1
-                segment_dict['stop_index']=segment[i]
-            self.lc.segmented.append([segment_dict])
+            if self.lc.full['time'][segment[i]]-self.lc.full['time'][segment[i-1]+1]>min_segment_length:
+                segment_mask[segment[i-1]+1:segment[i]+1]=segment_number
+                segment_number+=1
+        
+        self.lc.segment=segment_mask
+
+
+    def add_flare1(self,tpeak,fwhm,ampl,q_flags=None,segments=None):
+        mask=self.lc.get_mask(q_flags=q_flags, segments=segments)
+        t=self.lc.full['time'][mask]
+        flux=self.lc.full['flux'][mask]
+        flux_err=self.lc.full['flux_err'][mask]
+        quality=self.lc.full['quality'][mask]
+        flare=aflare1(t,tpeak=tpeak, fwhm=fwhm, ampl=ampl)
+        self.flares.lc={"time":t, "flux":flux+flare,
+                        "flux_err":flux_err, "quality":quality}
--- imports.py
+++ imports.py
@@ -9,3 +9,4 @@ import pymc3 as pm
 import pymc3_ext as pmx
 import aesara_theano_fallback.tensor as tt 
 from celerite2.theano import terms, GaussianProcess
+from aflare import aflare, aflare1
