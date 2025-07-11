84d1f93bc4a0eeace4c7ce55e5b6ef9bd19ea91c
vaammpyy <rohankumarprasad@yahoo.com>
2025-07-11 22:54:00 +0530

    Adding pipeline test codes.

---
+++ findflares/tests/findflare.py
@@ -0,0 +1,11 @@
+from findflares.pipeline_utils import run_pipeline_py
+
+candidates_file="./stars_list.txt"
+telescope="TESS"
+data_dir="./output"
+output_dir=data_dir
+CPU_CORES=2
+INJREC=0
+redo=True
+
+run_pipeline_py(candidates_file, telescope, data_dir,output_dir,CPU_CORES, redo=redo, injrec=INJREC)
