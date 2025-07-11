b05c18456055720f80fb2de3d83d54e82998a8e4
vaammpyy <rohankumarprasad@yahoo.com>
2025-07-12 00:43:27 +0530

    Added boolean functionality to argparser for RERUN

--- findflares/run_pipeline.py
+++ findflares/run_pipeline.py
@@ -16,9 +16,19 @@ parser.add_argument('-d', "--datadir",
                     type=str,
                     help="Path to store the output")
 
-parser.add_argument('-r', "--rerun",
-                    action='store_true',
-                    help='Re-run pipeline for all existing stars.')
+# parser.add_argument('-r', "--rerun",
+#                     action='store_true',
+#                     help='Re-run pipeline for all existing stars.')
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("yes", "true", "t", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "0"):
+        return False
+    raise argparse.ArgumentTypeError("Boolean value expected.")
+
+parser.add_argument('--rerun', type=str2bool, default=False, help='True to rerun the pipeline for already processed data.')
 
 parser.add_argument('-i', '--injrec',
                     type=int,
